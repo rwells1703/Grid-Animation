2586cfb3228dfbcc67ff14d6c63599391f813720
Richard Wells <rwells1703@gmail.com>
2020-03-26 13:03:20 +0000

    Some refactoring and commenting.

--- Grid.java
+++ Grid.java
@@ -3,22 +3,26 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.RowConstraints;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.animation.KeyFrame;
 import javafx.util.Duration;
 import javafx.animation.Timeline;
-import javafx.animation.KeyFrame;
 
 public class Grid {
   private final GridPane gridPane;
   private final int dimension;
-  private int t = 0;
+  private int time = 0;
 
   public Grid(int dimension, double windowSize) {
     this.dimension = dimension;
 
+    // Set the size of the grid to fit the window
     gridPane = new GridPane();
     gridPane.setMaxHeight(windowSize);
     gridPane.setMaxWidth(windowSize);
 
+    // Add column/row constraints to make all grid cells take an equal share of space
     for (int i = 0; i < dimension; i++) {
       ColumnConstraints columnConstraints = new ColumnConstraints();
       columnConstraints.setPercentWidth(100f/dimension);
@@ -29,12 +33,17 @@ public class Grid {
       gridPane.getRowConstraints().add(rowConstraints);
     }
 
+    // Create each cell in the grid
     for (int col = 0; col < dimension; col++) {
       for (int row = 0; row < dimension; row++) {
+        // Creates a new cell
         FlowPane cell = new FlowPane();
 
+        // Ensures each cell will fill its slot in the grid
         cell.setPrefHeight(Double.POSITIVE_INFINITY);
         cell.setPrefWidth(Double.POSITIVE_INFINITY);
+
+        // Adds the cell to the grid
         gridPane.add(cell, col, row);
       }
     }
@@ -44,69 +53,104 @@ public class Grid {
     return gridPane;
   }
 
+  // Returns the cell at that exact position in the grid
   private Node getCell(int col, int row) {
     return gridPane.getChildren().get(row + col * dimension);
   }
 
-  private Node getCellInShell(int n, int shell) {
-    int min = shell;
-    int max = dimension-1 - shell;
+  // Returns the cell that is n positions in from the start
+  // Each ring, is a loop of cells around the grid
+  // Ring 0 is the outer loop of cells
+  // Higher ring numbers move further into the center
+  private Node getCellInRing(int n, int ring) {
+    // Set the maximum and minimum row and column numbers
+    // This is based on which ring we are currently highlighting
+    int min = ring;
+    int max = dimension-1 - ring;
 
+    // The size of each side of the current ring
     int sideLength = max - min;
 
+    // If we have not reached the center (a single cell for odd sized grids)
     if (sideLength != 0) {
+      // Get the current side of the ring we are on
       int side = n / sideLength;
 
       if (side == 0) {
-        // Top
+        // Top side of the ring
         return getCell(min + n, min);
       } else if (side == 1) {
-        // Right
+        // Right side of the ring
         return getCell(max, min + n % sideLength);
       } else if (side == 2) {
-        // Bottom
+        // Bottom side of the ring
         return getCell(max - n % sideLength, max);
       } else if (side == 3) {
-        // Left
+        // Left side of the ring
         return getCell(min, max - n % sideLength);
       }
     }
 
-    return getCell(shell, shell);
+    // Otherwise return the central cell of the grid (the final cell to be highlighted)
+    return getCell(ring, ring);
   }
 
-  private int getShellSize(int shell) {
-    int length = dimension - 2*shell;
+  // Get the size of a ring
+  private int getRingSize(int ring) {
+    // The length of one side of the ring
+    int length = dimension - 2*ring;
 
+    // If the ring is a single cell
     if (length == 1)
       return 1;
 
+    // If the ring is multiple cells
     return 4*(length - 1);
   }
 
-  private Node getHighlightedCell(int t) {
-    for (int shell = 0; shell <= dimension/2; shell++) {
-      int shellSize = getShellSize(shell);
+  // Takes a time parameter and highlights the correct cell accordingly
+  private Node getHighlightedCell(int time) {
+    // Move from the outer ring to the inner ring
+    for (int ring = 0; ring <= dimension/2; ring++) {
+      // Get the number of cells in the ring
+      int ringSize = getRingSize(ring);
 
-      if (t < shellSize) {
-        return getCellInShell(t, shell);
+      // If the current cell is in this ring
+      if (time < ringSize) {
+        // Highlight the current cell
+        return getCellInRing(time, ring);
       }
 
-      t -= shellSize;
+      // Otherwise move onto the next ring
+      time -= ringSize;
     }
 
+    // The time provided has no corresponding cell
     return null;
   }
 
+  // Handles the animation of cell highlighting
   public void animate() {
-    Timeline timeline = new Timeline(
-      new KeyFrame(Duration.millis(50), e -> {
-        getHighlightedCell(t).setStyle("-fx-background-color: green");
-        t++;
-      })
-    );
+    // Handles every time a new frame of the animation is needed
+    EventHandler<ActionEvent> highlightCell = new EventHandler<>() {
+      // Updates the grid highlighting for the next cell along
+      @Override
+      public void handle(ActionEvent e) {
+        // Sets the background of the cell to green
+        getHighlightedCell(time).setStyle("-fx-background-color: green");
+        time++;
+      }
+    };
 
+    // Next frame of animation occurs every 50ms
+    KeyFrame keyframe = new KeyFrame(Duration.millis(50), highlightCell);
+
+    // Creates an animation timeline
+    Timeline timeline = new Timeline(keyframe);
+
+    // The animation continues until the entire grid is filled up (all NxN cells) before stopping
     timeline.setCycleCount(dimension*dimension);
+    // Begins the animation
     timeline.play();
   }
 }
--- Main.java
+++ Main.java
@@ -8,24 +8,32 @@ import javafx.stage.Stage;
 public class Main extends Application {
   @Override
   public void start(Stage stage) {
+    // Creates a root pane for all other nodes
     FlowPane root = new FlowPane();
 
+    // Get the minimum dimension of the screen (either the width or height)
+    // The size of the window will be 80% of this dimension
     Rectangle2D screenBounds = Screen.getPrimary().getBounds();
     double windowSize = screenBounds.getWidth()*0.8;
     if (screenBounds.getWidth() > screenBounds.getHeight()) {
       windowSize = screenBounds.getHeight()*0.8;
     }
 
+    // The size of the grid
     int dimension = 9;
 
+    // Create the grid and add it to the root pane
     Grid grid = new Grid(dimension, windowSize);
     root.getChildren().add(grid.getGridPane());
 
+    // Create the scene
     Scene scene = new Scene(root, windowSize, windowSize);
     stage.setScene(scene);
 
+    // Show the window on the screen
     stage.show();
 
+    // Animate the grid
     grid.animate();
   }
 
